87d7fd62e03f7ecc7804cd5eadb4e85093191194
AhmadOthman <trollingosman101@outlook.com>
2024-07-15 00:59:37 +0300

    update Post.php and User.php

--- app/Models/User.php
+++ app/Models/User.php
@@ -42,7 +42,7 @@ class User extends Authenticatable {
         return [
             'email_verified_at' => 'datetime:Y-m-d H:i:s',
             'password' => 'hashed',
-            'created_at' => 'datetime:Y-m-d H:i:s'
+            'created_at' => 'datetime:Y-m-d H:i:s A'
         ];
     }
 }
